7658db2bcde9df771e4e9cad4f510ad7120cd634
Ron <ronavraham1999@gmail.com>
2025-12-09 12:43:17 +0200

    commmit

--- Assets/Scripts/0-bfs/TilemapGraph.cs
+++ Assets/Scripts/0-bfs/TilemapGraph.cs
@@ -1,33 +1,41 @@
-﻿using System.Linq;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 /**
  * A graph that represents a tilemap, using only the allowed tiles.
  */
-public class TilemapGraph: IGraph<Vector3Int> {
-    private Tilemap tilemap;
-    private TileBase[] allowedTiles;
+public class TilemapGraph : IGraph<Vector3Int>
+{
+    private readonly Tilemap _tilemap;
+    private readonly TileBase[] _allowedTiles;
 
-    public TilemapGraph(Tilemap tilemap, TileBase[] allowedTiles) {
-        this.tilemap = tilemap;
-        this.allowedTiles = allowedTiles;
-    }
-
-    static Vector3Int[] directions = {
-            new Vector3Int(-1, 0, 0),
-            new Vector3Int(1, 0, 0),
-            new Vector3Int(0, -1, 0),
-            new Vector3Int(0, 1, 0),
+    private static readonly Vector3Int[] Directions =
+    {
+        new Vector3Int(-1, 0, 0),
+        new Vector3Int(1, 0, 0),
+        new Vector3Int(0, -1, 0),
+        new Vector3Int(0, 1, 0),
     };
 
-    public IEnumerable<Vector3Int> Neighbors(Vector3Int node) {
-        foreach (var direction in directions) {
+    public TilemapGraph(Tilemap tilemap, TileBase[] allowedTiles)
+    {
+        _tilemap = tilemap;
+        _allowedTiles = allowedTiles;
+    }
+
+    public IEnumerable<Vector3Int> Neighbors(Vector3Int node)
+    {
+        foreach (var direction in Directions)
+        {
             Vector3Int neighborPos = node + direction;
-            TileBase neighborTile = tilemap.GetTile(neighborPos);
-            if (allowedTiles.Contains(neighborTile))
+            TileBase neighborTile = _tilemap.GetTile(neighborPos);
+
+            if (_allowedTiles.Contains(neighborTile))
+            {
                 yield return neighborPos;
+            }
         }
     }
 }
